33bec86f83b631c2918cb5356414148c1990577f
Ashish77393 <ashishkumar77393@gmail.com>
2024-07-17 23:50:28 +0530

    this is small fronted e-commerce website

---
+++ script.js
@@ -0,0 +1,16 @@
+function f1() {
+  confirm("Check your Email and Password");
+}
+function f2() {
+  confirm("You see all details CareFully");
+}
+function f4(e) {
+  e.style.backgroundColor = "skyblue";
+  e.style.color = "red";
+  e.style.fontSize = "25px";
+}
+function f5(e) {
+  e.style.backgroundColor = "white";
+  e.style.color = "violet";
+  e.style.fontSize = "20px";
+}
